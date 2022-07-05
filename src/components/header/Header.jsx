6ed1b2f928df1ee605a1d22e580b0449fa86997e
bilginmrv <mrvblgn44@gmail.com>
2022-07-05 17:38:48 +0300

    Create header component jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
-function header() {
+function Header() {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header_container">
+         <h5>Hello I'm</h5>
+         <h1>Merve Bilgin</h1>
+         <h5 className='text-light'>Fullstack Developer</h5>
+         <CTA/>
+
+         <div className="me">
+          <img src={ME} alt='me'/>
+         </div>
+
+         <a href='#contact'className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
-export default header
+export default Header
