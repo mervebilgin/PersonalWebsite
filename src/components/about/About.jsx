a5ef230b1e6eded3cb1cd0dbc45caf9e60491ad9
bilginmrv <mrvblgn44@gmail.com>
2022-07-06 12:26:31 +0300

    Create about component css

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,7 +10,7 @@ function About() {
   return (
     <section id='about'>
       <h5>Get To Know</h5>
-      <h2>About Ma</h2>
+      <h2>About Me</h2>
 
       <div className="container about_container">
         <div className='about_me'>
