8c9f5c59acdb0010615cabef1c38daa78a15136a
bilginmrv <mrvblgn44@gmail.com>
2022-07-08 20:44:45 +0300

    Create footer component css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,14 +8,14 @@ function Footer() {
   return (
     <footer>
       <a href="#" className='footer_logo'>BILGIN</a>
-      
+
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="<#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#contact">C ontact</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
 
       <div className="footer_socials">
