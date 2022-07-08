1f79e83e2d980db96242a3ebd229ee7cab214b5f
bilginmrv <mrvblgn44@gmail.com>
2022-07-08 14:12:20 +0300

    Create carousel/slides

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,15 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const data = [
   {
     avatar: AVTR1,
@@ -34,27 +43,27 @@ function Testimonials() {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className='container testimonials_container'>
+      <Swiper className='container testimonials_container'
+      // install Swiper modules
+      modules={[ Pagination ]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      >
         {
           data.map(({avatar, name, review}, index) => {
             return (
-              <article className='testimonial'>
+              <SwiperSlide className='testimonial'>
                 <div className='client_avatar'>
                   <img src={avatar} alt='' />
                 </div>
-                <h5 className='client_name'>name</h5>
+                <h5 className='client_name'>{name}</h5>
                 <small className='client_review'>{review}</small>
-              </article>
+              </SwiperSlide>
             )
           })
         }
-
-
-
-      </div>
-
-
-    
+      </Swiper>
     </section>
   )
 }
